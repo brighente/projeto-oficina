0f71da83722c33b62654b739b1b9334e317b984a
João Vitor Brighente <jvbrighs@gmail.com>
2024-11-17 23:52:11 -0300

    responsividade tablet

--- script/script.js
+++ script/script.js
@@ -1,19 +1,29 @@
+            // CARROSSEL
+
 let indiceAtual = 0;
 
-        function moverCarrossel(direcao) {
-            const slides = document.querySelectorAll('.carrossel-slide .slide');
-            const totalSlides = slides.length;
+    function moverCarrossel(direcao) {
+        const slides = document.querySelectorAll('.carrossel-slide .slide');
+        const totalSlides = slides.length;
 
-            // Calculando o novo índice
-            indiceAtual += direcao;
+        // Calculando o novo índice
+        indiceAtual += direcao;
 
-            if (indiceAtual < 0) {
-                indiceAtual = totalSlides - 1; // Vai para o último slide
-            } else if (indiceAtual >= totalSlides) {
-                indiceAtual = 0; // Vai para o primeiro slide
-            }
-
-            // Movendo o carrossel
-            const carrossel = document.getElementById('carrossel');
-            carrossel.style.transform = `translateX(-${indiceAtual * 100}%)`;
+        if (indiceAtual < 0) {
+            indiceAtual = totalSlides - 1; // Vai para o último slide
+        } else if (indiceAtual >= totalSlides) {
+            indiceAtual = 0; // Vai para o primeiro slide
         }
+
+        // Movendo o carrossel
+        const carrossel = document.getElementById('carrossel');
+        carrossel.style.transform = `translateX(-${indiceAtual * 100}%)`;
+    }
+
+
+            // BOTAO VOLTAR AO TOPO
+
+    function voltarTopo() {
+        window.scroll(0, 0);
+        }
+              
