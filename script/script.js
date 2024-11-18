da7898c46864ad30e807fe01be6e31852f7f6a7a
João Vitor Brighente <jvbrighs@gmail.com>
2024-11-18 13:07:46 -0300

    finalização

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
             // CARROSSEL
 
 let indiceAtual = 0;
+let bodycheck = document.getElementById('body')
 
     function moverCarrossel(direcao) {
         const slides = document.querySelectorAll('.carrossel-slide .slide');
@@ -26,4 +27,35 @@ let indiceAtual = 0;
     function voltarTopo() {
         window.scroll(0, 0);
         }
+
+
+            // MENU HAMBURGER VERIFICAÇÃO
+
+    body.onresize = mudouTamanho;
+
+    function mudouTamanho(){
+        if(window.innerWidth >= 1024){
+            menu.style.display = 'flex';
+        } else {
+            menu.style.display = 'none'
+        }
+    }
+
+
+        
+    
+
+
+    function clickMenu(){
+        if(menu.style.display == 'block'){
+            menu.style.display = 'none'
+            header.style.display = 'flex'
+        } else {
+            menu.style.display = 'block'
+            header.style.display = 'grid'
+        }
+    }
+
+
+        
               
